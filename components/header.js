fc97b4364fa25a01f8d0296c1f158601eeae5bc4
TmRAaEx <tmr.aaex@gmail.com>
2024-12-10 09:00:52 +0100

    Additional styling for the topbar. Also started on adding the navigation field

--- components/header.js
+++ components/header.js
@@ -7,41 +7,48 @@ class HeaderComp extends HTMLElement {
     console.log("Connected");
     this.shadowRoot.innerHTML = `
     <style>
-      *{
-        box-sizing: border-box;
-        }
- 
-        header{ 
-            width: 100vw;
+         :host {
+            display: block;
+            width: 100%;
+            background-color: #001524;
             border-bottom: 1px solid black;
             padding: 5px;
+            box-sizing: border-box;
             height: 100px;
+          }
+
+          #topbar{
             display: flex;
+            width: 100%;
+            height: 100%;
             justify-content: center;
             align-items: center;
-            background-color: #001524;
-        }
-
-        #searchbar-wrapper{
+          }
+        
+          #searchbar-wrapper {
             width: 50%;
             max-width: 500px;
             min-width: 200px;
-        }
+           
+          }
 
-        #search-input{
+          #search-input {
             width: 100%;
-            height: 35px;
-            border-radius: 10px;
+            height: 45px;
+            border-radius: 15px;
             border: 1px solid black;
             padding: 8px;
             font-size: clamp(10pt, 5vw, 14pt);
-        }
+          }
     </style>
 
     <header id="topbar">
       <div id="searchbar-wrapper">
         <input type="search" placeholder="Sök..." aria-label="sök" id="search-input">
       </div>
+      <nav>
+      
+      </nav>
     </header>
     `;
   }

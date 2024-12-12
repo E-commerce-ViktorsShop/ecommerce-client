e3d78eefba4a66621a7ddec3ff93c770573a3238
viktorlinne <viktor.linne@medieinstitutet.se>
2024-12-12 11:30:52 +0100

    Revert "Ec 25 migrate header"

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -3,15 +3,11 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.css'
 import HomePage from "./pages/home.jsx";
-// Components
-import HeaderComp from "./components/header.jsx";
 
 function App() {
 
     return (
         <BrowserRouter>
-            <HeaderComp/>
-
             <Routes>
                 <Route path="/" element={<HomePage/>}></Route>
             </Routes>

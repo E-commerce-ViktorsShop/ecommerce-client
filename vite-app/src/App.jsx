f8aac359bbda369b19fa8e47d35f9a75fad4267e
Viktor Linné <viktor.linne@medieinstitutet.se>
2024-12-12 11:58:09 +0100

    migrated html and css to react

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -4,14 +4,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 import HomePage from "./pages/home.jsx";
 
+import FooterComp from "./components/footer.jsx";
+
 function App() {
 
     return (
         <BrowserRouter>
+
             <Routes>
                 <Route path="/" element={<HomePage/>}></Route>
             </Routes>
+            <FooterComp/>
         </BrowserRouter>
+
     )
 }
 

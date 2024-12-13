2629556748d686db25c1fd00b4c9c3d9461d47ab
Viktor Linné <viktor.linne@medieinstitutet.se>
2024-12-13 10:22:45 +0100

    Added categories to header

--- vite-app/src/components/header.jsx
+++ vite-app/src/components/header.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import '../styles/header.css'; // CSS styles moved to an external file or styled-components
 
 export default function HeaderComp() {
+
+    const categories = [
+        'Apple',
+        'Datorer & Tillbehör',
+        'Datorkomponenter',
+        'Hem & Hälsa',
+        'Leksaker & Hobby',
+        'Mobil',
+        'Nätverk & Smarta Hem',
+        'Spel',
+        'TV, Ljud & Bild'
+    ]
     return (
         <header className="topbar">
             {/* Search Bar Section */}
@@ -17,12 +29,7 @@ export default function HeaderComp() {
             {/* Navigation Section */}
             <nav className="navigation">
                 <ul className="category-list">
-                    <li><a href="#" className="category-link">Category</a></li>
-                    <li><a href="#" className="category-link">Category</a></li>
-                    <li><a href="#" className="category-link">Category</a></li>
-                    <li><a href="#" className="category-link">Category</a></li>
-                    <li><a href="#" className="category-link">Category</a></li>
-                    <li><a href="#" className="category-link">Category</a></li>
+                    {categories.map((category, index) => (<li className="category-item" key={index}><a className="category-link" href={`/categories/${category}`}>{category}</a></li>))}
                 </ul>
             </nav>
         </header>

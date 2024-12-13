822515f6052515c2485fcc3d26b6bb1096832117
David Funck <xdavidfunckx@gmail.com>
2024-12-13 12:42:54 +0100

    Update home.jsx

--- vite-app/src/pages/home.jsx
+++ vite-app/src/pages/home.jsx
@@ -1,48 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { fetchProducts } from '../utils/functions';
 
 export default function HomePage() {
 	const [searchValue, setSearchValue] = useState('spider-man');
 	const [results, setResults] = useState([]);
 	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
 
-	const apiKey = '7db2dcc4';
-
-	// Fetch search results from OMDb API
-	const searchOMDb = async (searchValue) => {
-		const url = `https://www.omdbapi.com/?s=${encodeURIComponent(
-			searchValue
-		)}&apikey=${apiKey}`;
-		setLoading(true);
-		setError(null);
-
-		try {
-			const response = await fetch(url);
-			if (!response.ok) {
-				throw new Error('Empty');
-			}
-			const data = await response.json();
-
-			if (data.Response === 'True') {
-				setResults(data.Search.slice(0, 5)); // Get first 5 results
-			} else {
-				throw new Error(data.Error);
-			}
-		} catch (error) {
-			setError(error.message);
-		} finally {
-			setLoading(false);
-		}
-	};
+	const apiKey = '';
 
 	// Fetch results on initial render or when searchValue changes
 	useEffect(() => {
-		searchOMDb(searchValue);
-	}, [searchValue]);
+		async function fetchData() {
+			const data = await fetchProducts();
+			setProducts(data);
+			return data;
+		}
 
-	const handleSearchChange = (event) => {
-		setSearchValue(event.target.value);
-	};
+		fetchData().then((data) => {
+			console.log(data);
+		});
+	}, []);
 
 	return (
 		<>

27e686c5aba5c46bac8148be22c04fabe518afdc
David Funck <xdavidfunckx@gmail.com>
2024-12-16 11:36:53 +0100

    Added search results to home page
    Also added some styling to product component.

--- vite-app/src/pages/home.jsx
+++ vite-app/src/pages/home.jsx
@@ -1,36 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { fetchProducts } from '../utils/functions.js';
+import { fetchProducts, fetchProductsBySearch } from '../utils/functions.js';
 import ProductComp from '../components/product.jsx';
+import { useLocation } from 'react-router';
 
 export default function HomePage() {
-	const [products, setProducts] = useState([]);
+	const [filteredProducts, setFilteredProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const location = useLocation();
+
+	async function fetchData(query = '', page = 1, limit = 10) {
+		setLoading(true);
+		try {
+			let data = [];
+			if (query) {
+				data = await fetchProductsBySearch(query, page, limit);
+			} else {
+				data = await fetchProducts();
+			}
+
+			setFilteredProducts(Array.isArray(data) ? data : []);
+		} catch (error) {
+			console.error('Error fetching products:', error);
+
+			setFilteredProducts([]);
+		} finally {
+			setLoading(false);
+		}
+	}
 
 	useEffect(() => {
-		async function fetchData() {
-			const data = await fetchProducts();
-			setProducts(data);
-			return data;
-		}
+		const queryParams = new URLSearchParams(location.search);
+		const searchTerm = queryParams.get('searchTerm') || '';
+		console.log('Search Term:', searchTerm);
 
-		fetchData().then((data) => {
-			console.log(data);
-		});
-	}, []);
+		fetchData(searchTerm);
+	}, [location.search]);
 
 	return (
 		<>
 			<main>
 				<h1 className='text-center mb-5'>Välkommen!</h1>
 				<h2 className='text-center mb-5'>Kolla in dessa produkter</h2>
-				<ul
-					id='product-list'
-					className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3'
-				>
-					{products.map((product) => (
-						<ProductComp key={product._id} product={product} />
-					))}
-				</ul>
+
+				{/* Loading state */}
+				{loading ? (
+					<p className='text-center'>Laddar..</p>
+				) : (
+					<ul
+						id='product-list'
+						className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3'
+					>
+						{filteredProducts.length > 0 ? (
+							filteredProducts.map((product) => (
+								<ProductComp key={product._id} product={product} />
+							))
+						) : (
+							<li>No products found</li>
+						)}
+					</ul>
+				)}
 			</main>
 		</>
 	);

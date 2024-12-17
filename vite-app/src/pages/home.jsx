d2d7fc4d1818bbb542e4bab9e176504d9beeebbd
David Funck <xdavidfunckx@gmail.com>
2024-12-17 09:43:14 +0100

    Added styling to main page

--- vite-app/src/pages/home.jsx
+++ vite-app/src/pages/home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { fetchProducts } from '../utils/functions.js';
 import ProductComp from '../components/product.jsx';
+/* import banner from '../assets/rb_981.png'; */
 
 export default function HomePage() {
 	const [products, setProducts] = useState([]);
@@ -20,8 +21,10 @@ export default function HomePage() {
 
 	return (
 		<>
-			<main>
-				<h1 className='text-center mb-5'>Välkommen!</h1>
+			{/* <img className='w-100' src={banner} alt='Christmas banner' /> */}
+
+			<main className='container-lg' style={{ background: '#f6f6f6' }}>
+				<h1 className='text-center mt-5 mb-5'>Välkommen!</h1>
 				<h2 className='text-center mb-5'>Kolla in dessa produkter</h2>
 				<ul
 					id='product-list'

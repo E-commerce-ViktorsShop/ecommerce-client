d6d4dbc6179076f712b9c95d13d40d9dfbe52f7f
David Funck <xdavidfunckx@gmail.com>
2024-12-18 10:22:25 +0100

    Solved scroll to top issue and updated banner

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './App.css'
-import HomePage from "./pages/home.jsx";
+import './App.css';
+import HomePage from './pages/home.jsx';
 
-import FooterComp from "./components/footer.jsx";
-import ProductPage from "./pages/product.jsx";
-import CategoryPage from "./pages/category.jsx";
+import FooterComp from './components/footer.jsx';
+import ProductPage from './pages/product.jsx';
+import CategoryPage from './pages/category.jsx';
 
 // Components
-import HeaderComp from "./components/header.jsx";
+import HeaderComp from './components/header.jsx';
+import ScrollToTop from './utils/scrollToTop.jsx';
 
 function App() {
-
-    return (
-        <BrowserRouter>
-            <HeaderComp/>
-            <Routes>
-                <Route path="/" element={<HomePage/>}></Route>
-                <Route path="/product/:id" element={<ProductPage/>}></Route>
-                <Route path="/categories/:name" element={<CategoryPage/>}></Route>
-            </Routes>
-            <FooterComp/>
-        </BrowserRouter>
-
-    )
+	return (
+		<BrowserRouter>
+			<ScrollToTop />
+			<HeaderComp />
+			<Routes>
+				<Route path='/' element={<HomePage />}></Route>
+				<Route path='/product/:id' element={<ProductPage />}></Route>
+				<Route path='/categories/:name' element={<CategoryPage />}></Route>
+			</Routes>
+			<FooterComp />
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;

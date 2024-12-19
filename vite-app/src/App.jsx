e1630f3f46e3014ed92b0a68b01ad24362f4d4f5
TmRAaEx <tmr.aaex@gmail.com>
2024-12-19 14:27:46 +0100

    Added productNotFound.jsx and productError.jsx

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -3,15 +3,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/home.jsx';
-
-import FooterComp from './components/footer.jsx';
+// pages
 import ProductPage from './pages/product.jsx';
 import CategoryPage from './pages/category.jsx';
+import PageNotFound from "./pages/404.jsx";
+import ProductNotFound from "./pages/productNotFound.jsx";
+import ProductError from "./pages/productError.jsx";
 
 // Components
 import HeaderComp from './components/header.jsx';
+import FooterComp from './components/footer.jsx';
+
 import ScrollToTop from './utils/scrollToTop.jsx';
-import PageNotFound from "./pages/404.jsx";
 
 function App() {
     return (
@@ -20,6 +23,8 @@ function App() {
             <HeaderComp/>
             <Routes>
                 <Route path='/' element={<HomePage/>}></Route>
+                <Route path='/product/404' element={<ProductNotFound/>}></Route>
+                <Route path='/product/error' element={<ProductError/>}></Route>
                 <Route path='/product/:id' element={<ProductPage/>}></Route>
                 <Route path='/categories/:name' element={<CategoryPage/>}></Route>
                 <Route path='*' element={<PageNotFound/>}></Route>
---
+++ vite-app/src/pages/productNotFound.jsx
@@ -0,0 +1,25 @@
+export default function ProductNotFound() {
+    return (
+        <>
+            <main className="p-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="text-center">
+                                <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
+                                    <span className="display-1 fw-bold">4</span>
+                                    <span className="display-1 fw-bold bsb-flip-h">0</span>
+                                    <span className="display-1 fw-bold bsb-flip-h">4</span>
+                                </h2>
+                                <h3 className="h2 mb-2">Hoppsan!</h3>
+                                <p className="mb-5">Produkten du letar efter finns inte.</p>
+                                <a className="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0" href="/"
+                                   role="button">Gå tillbaka hem</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}

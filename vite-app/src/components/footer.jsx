51880002e5f583926cbc3560fc149cd282efeb1f
linne <viktor.linne@gmail.com>
2024-12-19 12:43:10 +0100

    Added last styling to footer. Added logo and removed beige section of footer

--- vite-app/src/components/footer.jsx
+++ vite-app/src/components/footer.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import { categories } from '../utils/static.js';
+import {categories} from '../utils/static.js';
 
 export default function FooterComp() {
     return (
-        <footer className="bg-primary text-light d-flex flex-column align-items-center" style={{ minHeight: '395px' }}>
-            {/* Footer Navigation Links */}
-            <div className="container d-flex justify-content-center align-items-center flex-grow-1">
-                <div className="row">
+        <footer className="bg-primary text-light d-flex flex-column align-items-center"
+                style={{
+                    minHeight: 'fit-content',
+                    fontFamily: '"Comic Sans MS", cursive, sans-serif',
+                }}>
+
+            <div className="container d-flex flex-column flex-grow-1">
+                <div className="row align-items-center">
+
+                    {/* Page logo to the right */}
                     <div className="col text-center">
+                        <img src="/logos/Viktorsshop-comic.png" alt="logo for page"/>
+                    </div>
+
+                    {/* Link list in the center */}
+                    <div className="col text-center"
+                         style={{paddingTop: '1rem',
+                                 paddingBottom: '1rem'}}>
                         <ul className="list-unstyled m-0 p-0">
                             {categories.map((category) => (
                                 <li key={category} className="py-1">
                                     <a
                                         href={`/categories/${category}`}
                                         className="text-light text-decoration-none"
+
                                     >
                                         {category}
                                     </a>
@@ -22,32 +36,24 @@ export default function FooterComp() {
                             ))}
                         </ul>
                     </div>
-                </div>
-            </div>
 
-            {/* Contact Section */}
-            <div className="bg-light text-dark py-3 w-100">
-                <div className="container">
-                    <div className="row text-center">
-                        <div className="col">
-                            <div>0705788520</div>
-                        </div>
-                        <div className="col">
-                            <div>Viktors Väg 123</div>
-                        </div>
-                        <div className="col">
-                            <a
-                                href="mailto:viktor.linne@gmail.com"
-                                className="text-dark text-decoration-none"
-                            >
-                                viktorshop@gmail.com
-                            </a>
-                        </div>
+                    {/* Kontakt Section */}
+                    <div className="col text-center">
+                        <h5>Kontakt</h5>
+                        <a
+                            href="mailto:viktorshop@gmail.com"
+                            className="text-light text-decoration-none"
+
+                        >
+                            viktorshop@gmail.com
+                        </a>
+                        <p className="mb-0">
+                            0705788520
+                        </p>
                     </div>
                 </div>
             </div>
 
-            {/* Rights Reserved Section */}
             <div className="bg-dark text-light py-2 w-100">
                 <div className="container text-center">
                     <p className="mb-0">© 2024 ViktorShop. All Rights Reserved.</p>
